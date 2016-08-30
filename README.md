aa4839ab6bde5b45b0b372960847a5a6927353dc
Levi Smith <levi@fynx.me>
2016-08-30 13:49:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Windows Scripts for Jenkins
+
+## Script List
+
+* Jenkins Swarm - Dynamically Connect Jenkins Windows Slaves using Powershel and Swarm
+* Jenkins Swarm (AWS) - Same as above but specific to AWS
---
+++ swarm-slave-connect-aws.ps1
@@ -0,0 +1,19 @@
+Set-ExecutionPolicy Unrestricted
+
+$version = "2.2"
+$swarmURL = "https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/$version/swarm-client-$version-jar-with-dependencies.jar"
+$swarmClient = "C:\Jenkins\swarm-client-$version.jar"
+$masterURL = "127.0.0.1:8080"
+$instanceID = invoke-restmethod -uri http://169.254.169.254/latest/meta-data/instance-id
+$instanceName = "windows-$instanceID"
+$executors = "3"
+
+# Pull swarm client w/ dependecies
+Write-Output "Downloading jenkins swarm client "
+
+Start-BitsTransfer -Source $swarmURL -Destination $swamClient
+
+Write-Output "Executing swarm process "
+
+# expect java.exe in the PATH, and use -noCertificateCheck to skip SSL validation
+& java -jar $swarmClient -disableSslVerification -labels "Windows" -master $masterURL -name $instanceName -executors $executors
